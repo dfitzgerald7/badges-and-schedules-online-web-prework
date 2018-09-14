d25efc1e3776e5e5ae8778e9642bd9581b847c43
Doug Fitzgerald <dfitzge1@conncoll.edu>
2018-09-14 15:53:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,14 @@ end
 
 def batch_badge_creator(name_array)
   badge_array = []
-  name_array.each do
-    
+  name_array.each do |name|
+    badge_array << "Hello, my name is #{name}"
+  end 
+end 
+
+def assign_rooms(array)
+  room_array =[]
+  array.each.with_index(1) do |name, index|
+    room_array << "Hello, #{name}! You'll be assigned to room #{index}!"
   end 
 end 
